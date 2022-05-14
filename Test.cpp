c182b66db16b2be43e54a931b51b7e87faf071ce
Eran <eranki.katz@gmail.com>
2022-05-14 16:14:21 +0000

    Tests done

---
+++ Test.cpp
@@ -0,0 +1,69 @@
+#include "doctest.h"
+#include "OrgChart.hpp"
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <iterator>
+using namespace std;
+using namespace ariel;
+
+TEST_CASE("Bad"){
+     OrgChart organization;
+     CHECK_THROWS(organization.add_sub("CEO", "CFO"));
+     CHECK_THROWS(organization.add_sub("CTO", "VP_SW"));
+     CHECK_THROWS(organization.add_sub("COO", "VP_BI"));
+     CHECK_THROWS(organization.add_sub("CFO", "VP_BI"));
+    
+
+     organization = organization.add_root("CEO").add_sub("CEO", "CFO");
+}
+
+
+TEST_CASE("Good"){
+   OrgChart organization;
+     organization.add_root("CEO")
+      .add_sub("CEO", "CTO")         // Now the CTO is subordinate to the CEO
+      .add_sub("CEO", "CFO")         // Now the CFO is subordinate to the CEO
+      .add_sub("CEO", "COO")         // Now the COO is subordinate to the CEO
+      .add_sub("CTO", "VP_SW"); // Now the VP Software is subordinate to the CTO
+    vector<string> s;
+     size_t i = 0;
+    OrgChart::Iterator it;
+    for (it = organization.begin_level_order(); it != organization.end_level_order(); ++it)
+  {
+    s.push_back(*it);
+  }
+   CHECK(s[0] == "CEO");
+   CHECK(s[1] == "CTO");
+   CHECK(s[2] == "CFO");
+   CHECK(s[3] == "COO");
+   CHECK(s[4] == "VP_SW");
+  for ( it = organization.begin_reverse_order(); it != organization.reverse_order(); ++it)
+  {
+    s.push_back(*it);
+  }
+   CHECK(s[0] == "VP_SW");
+   CHECK(s[1] == "CTO");
+   CHECK(s[2] == "CFO");
+   CHECK(s[3] == "COO");
+   CHECK(s[4] == "CEO");
+   for ( it = organization.begin_preorder(); it != organization.end_preorder(); ++it)
+  {
+    s.push_back(*it);
+  }
+  CHECK(s[0] == "CEO");
+   CHECK(s[1] == "CTO");
+   CHECK(s[2] == "VP_SW");
+   CHECK(s[3] == "CFO");
+   CHECK(s[4] == "COO");
+   for (auto element : organization)
+  { 
+    s.push_back(*it);
+  }
+   CHECK(s[0] == "CEO");
+   CHECK(s[1] == "CTO");
+   CHECK(s[2] == "CFO");
+   CHECK(s[3] == "COO");
+   CHECK(s[4] == "VP_SW"); 
+}
